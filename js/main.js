8774a4c274245b8ce9bf2f38c60f21dc6432b9b5
aki-710 <ijiwarubaasanishi@gmail.com>
2024-01-27 14:33:28 +0900

    wasureteta

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-//アコーディオンをクリックした時の動作
-$('.open_1').on('click', function)
+$(function($){
+    $('.more-button').on('click', function (){
+        var $openBox = $('.open-box', $(this).closest('.container'));
+        if ($('.open-box').is(':hidden')) {
+            $('.open-box').slideDown();
+            $(this).text('close');
+        } else {
+            $('.open-box').slideUp();
+            $(this).text('open');
+        }
+        
+    });
+
+    var open = $('.modal-open'),
+   container = $('.modal-container');
+
+    open.on('click',function(event){ 
+  container.addClass('active');
+  event.stopPropagation();
+});
+});
