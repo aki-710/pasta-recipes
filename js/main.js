eaf4e687d2d0099921cf8099820ae4c2130342d8
aki-710 <ijiwarubaasanishi@gmail.com>
2024-01-27 14:58:56 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-$(function($){
-    $('.more-button').on('click', function (){
-        var $openBox = $('.open-box', $(this).closest('.container'));
-        if ($('.open-box').is(':hidden')) {
-            $('.open-box').slideDown();
-            $(this).text('close');
-        } else {
-            $('.open-box').slideUp();
-            $(this).text('open');
-        }
-        
-    });
-
-    var open = $('.modal-open'),
-   container = $('.modal-container');
-
-    open.on('click',function(event){ 
-  container.addClass('active');
-  event.stopPropagation();
-});
+$(window).scroll(function() {
+    
 });
